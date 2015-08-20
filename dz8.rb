47f32bae4458a9e8f671d5eb3f4592be30e88d2c
Siedovolosij Serhij <sedovolosiy@gmail.com>
2015-08-20 14:44:13 +0300

    написал программу не до конца

---
+++ dz8.rb
@@ -0,0 +1,5 @@
+loop do
+print 'Введите идентификатор: '
+name = gets
+puts name =~/\A(@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([a-z\d\_]+)\Z/ || name =~/\A(@@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([A-Z\d\_]+)\Z/|| name =~/\A([a-z])+([a-z\d\_]+)\Z/  ? 'Правильно' : 'Неправильно'
+end
