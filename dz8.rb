7b479b9a0005e26c3505f555aa3cceb5f3e724be
Siedovolosij Serhij <sedovolosiy@gmail.com>
2015-08-20 16:56:35 +0300

    программа в виде метода

--- dz8.rb
+++ dz8.rb
@@ -1,5 +1,8 @@
-loop do
-print 'Введите идентификатор: '
-name = gets
-puts name =~/\A(@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([a-z\d\_]+)\Z/ || name =~/\A(@@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([A-Z\d\_]+)\Z/|| name =~/\A([a-z])+([a-z\d\_]+)\Z/  ? 'Правильно' : 'Неправильно'
+def name
+	loop do
+	print 'Введите идентификатор: '
+	name = gets
+	puts name =~/\A(@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([a-z\d\_]+)\Z/ || name =~/\A(@@[a-z])+([a-z\d\_]+)\Z/ || name =~/\A([A-Z])+([A-Z\d\_]+)\Z/|| name =~/\A([a-z])+([a-z\d\_]+)\Z/  ? 'Правильно' : 'Неправильно'
+	end
 end
+name
